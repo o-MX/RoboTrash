a1f04daba81f231c3f920fa0f6991760541dac19
Mcardenas29 <mcardenas@ucol.mx>
2021-10-22 11:33:48 -0500

    black window

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # Proyecto ṔI 1 UCOL
+import sys, pygame
+pygame.init()
 
-def main():
-    print("Hola")
-    
-if __name__ == "__main__":
-    main();
+size = width, height = 16*12, 16*11
+black = 0, 0, 0
+screen = pygame.display.set_mode(size)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+    screen.fill(black)
+    pygame.display.flip()
