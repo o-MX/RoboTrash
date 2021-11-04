7edc25ae50b8fcf361502ba04d8e2f078cee5291
Mcardenas29 <mcardenas@ucol.mx>
2021-11-04 13:23:20 -0600

    name changes

--- pantallas.py
+++ pantallas.py
@@ -6,7 +6,7 @@ class Inicio:
     def __init__(self):
         self.size = 192, 176
         self.surface = pygame.Surface(self.size)
-        self.bg = pygame.image.load("./Assets/TITLE_BG.png")
+        self.bg = pygame.image.load("./Assets/main_bg.png")
 
     def render(self):
         self.surface.blit(self.bg, (0, 0))
