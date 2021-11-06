d4d19544a8598725cf83c583a89ae546c4d31522
Mcardenas29 <mcardenas@ucol.mx>
2021-11-06 17:39:40 -0600

    moved textoanimado

--- pantallas.py
+++ pantallas.py
@@ -24,25 +24,3 @@ class Inicio:
         if(utils.Events.isKeyDown(pygame.K_RETURN)):
             self.game.changeStage(Inicio(self.game))
 
-class TextoAnimado:
-    def __init__(self):
-        self.size = 192, 176
-        self.opacidad = 0
-        self.milisegundos = 0
-        self.aumento = 10
-        self.font = pygame.font.Font("./Assets/dogicapixel.ttf", 8) 
-        self.text = self.font.render("Pulse Para Iniciar", 0, (0, 0, 0)) 
-        
-        
-    def render(self):
-        self.surface = pygame.Surface(self.size)
-        self.surface.blit(self.text, (40, 95)) 
-        
-
-    def update(self, dt):
-        self.milisegundos += dt
-        self.opacidad += self.aumento
-        self.opacidad = self.text.set_alpha(self.opacidad)
-        if self.milisegundos > 1000:
-            self.milisegundos = 0
-            self.aumento = -self.aumento
--- utils.py
+++ utils.py
@@ -14,3 +14,24 @@ class Events:
             if k.key == key:
                 return True
         return False
+
+class TextoAnimado:
+    def __init__(self):
+        self.size = 192, 176
+        self.opacidad = 0
+        self.milisegundos = 0
+        self.aumento = 10
+        self.font = pygame.font.Font("./Assets/dogicapixel.ttf", 8) 
+        self.text = self.font.render("Pulse Para Iniciar", 0, (0, 0, 0)) 
+
+    def render(self):
+        self.surface = pygame.Surface(self.size)
+        self.surface.blit(self.text, (40, 95)) 
+
+    def update(self, dt):
+        self.milisegundos += dt
+        self.opacidad += self.aumento
+        self.opacidad = self.text.set_alpha(self.opacidad)
+        if self.milisegundos > 1000:
+            self.milisegundos = 0
+            self.aumento = -self.aumento
