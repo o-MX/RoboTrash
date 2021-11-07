ca13ece13c4039818cd172c59cd62fc4aa25b866
Gilberto Valenzuela <junxd.98@gmail.com>
2021-11-07 14:03:17 -0600

    actualizo

--- pantallas.py
+++ pantallas.py
@@ -8,19 +8,19 @@ class Inicio:
         self.game = game
         self.bg = pygame.image.load("./Assets/main_bg.png")
         self.title = pygame.image.load("./Assets/title.png")
-        self.font = pygame.font.Font("./Assets/dogicapixel.ttf", 10)
-        self.text = self.font.render("Iniciar", False, (0, 0, 0))
         self.personje = pygame.image.load("./Assets/cabeza.png") 
         self.personje = pygame.transform.scale(self.personje, (35,28)) 
+        utils.TextoAnimado.__init__(self) #TRAIGO DE UTILS.PY EL TEXTO CON SUS PROPIEDADES
         
     def draw(self, vp):
         vp.blit(self.bg, (0, 0))
-        vp.blit(self.text, (80, 100))
         vp.blit(self.title, (20, 20))
-        vp.blit(self.personje, (70,120)) 
+        vp.blit(self.personje, (70,120))
+        utils.TextoAnimado.render(self,vp) #TRAIGO DE UTILS LA POSICION DEL TEXTO
 
 
     def update(self, dt):
         if(utils.Events.isKeyDown(pygame.K_RETURN)):
             self.game.changeStage(Inicio(self.game))
+        utils.TextoAnimado.update(self,dt)
 
--- utils.py
+++ utils.py
@@ -17,21 +17,19 @@ class Events:
 
 class TextoAnimado:
     def __init__(self):
-        self.size = 192, 176
         self.opacidad = 0
         self.milisegundos = 0
         self.aumento = 10
         self.font = pygame.font.Font("./Assets/dogicapixel.ttf", 8) 
-        self.text = self.font.render("Pulse Para Iniciar", 0, (0, 0, 0)) 
+        self.text = self.font.render("PULSE PARA INICIAR", 0, (0, 0, 0)) 
 
-    def render(self):
-        self.surface = pygame.Surface(self.size)
-        self.surface.blit(self.text, (40, 95)) 
+    def render(self, vp):
+        vp.blit(self.text, (40, 95))
 
     def update(self, dt):
+        self.opacidad = self.text.set_alpha(self.opacidad)
         self.milisegundos += dt
         self.opacidad += self.aumento
-        self.opacidad = self.text.set_alpha(self.opacidad)
         if self.milisegundos > 1000:
             self.milisegundos = 0
             self.aumento = -self.aumento
