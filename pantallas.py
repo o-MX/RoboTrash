736b6592ce78a7f5d2c2d5deee8a7d056dd38b11
Gilberto Valenzuela <junxd.98@gmail.com>
2021-11-08 20:58:40 -0600

    Finalicé la clase texto parpadeante

--- pantallas.py
+++ pantallas.py
@@ -1,3 +1,4 @@
+from typing import Text
 import pygame, sys
 import utils
 
@@ -9,18 +10,18 @@ class Inicio:
         self.bg = pygame.image.load("./Assets/main_bg.png")
         self.title = pygame.image.load("./Assets/title.png")
         self.personje = pygame.image.load("./Assets/cabeza.png") 
-        self.personje = pygame.transform.scale(self.personje, (35,28)) 
-        utils.TextoAnimado.__init__(self) #TRAIGO DE UTILS.PY EL TEXTO CON SUS PROPIEDADES
+        self.personje = pygame.transform.scale(self.personje, (35,28))
+        self.texto_A = utils.TextoAnimado("Presione x Para Iniciar", 1000)
+        self.texto_A.pos = (30, 100)
         
     def draw(self, vp):
         vp.blit(self.bg, (0, 0))
         vp.blit(self.title, (20, 20))
         vp.blit(self.personje, (70,120))
-        utils.TextoAnimado.render(self,vp) #TRAIGO DE UTILS LA POSICION DEL TEXTO
-
+        self.texto_A.render(vp)
 
     def update(self, dt):
         if(utils.Events.isKeyDown(pygame.K_RETURN)):
             self.game.changeStage(Inicio(self.game))
-        utils.TextoAnimado.update(self,dt)
+        self.texto_A.update(dt)
 
--- utils.py
+++ utils.py
@@ -16,20 +16,22 @@ class Events:
         return False
 
 class TextoAnimado:
-    def __init__(self):
+    def __init__(self,texto,tiempo):
         self.opacidad = 0
         self.milisegundos = 0
+        self.tiempo = tiempo
         self.aumento = 10
         self.font = pygame.font.Font("./Assets/dogicapixel.ttf", 8) 
-        self.text = self.font.render("PULSE PARA INICIAR", 0, (0, 0, 0)) 
+        self.text = self.font.render(texto, 0, (0, 0, 0)) 
+        self.pos = (0, 0)
 
     def render(self, vp):
-        vp.blit(self.text, (40, 95))
+        vp.blit(self.text, (self.pos))
 
     def update(self, dt):
-        self.opacidad = self.text.set_alpha(self.opacidad)
+        self.text.set_alpha(self.opacidad)
         self.milisegundos += dt
         self.opacidad += self.aumento
-        if self.milisegundos > 1000:
+        if self.milisegundos > self.tiempo:
             self.milisegundos = 0
             self.aumento = -self.aumento
